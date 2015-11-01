de96c449bae6136ce9522828882ec2a18051f6a2
Hunter Richards <hunter@counsyl.com>
2015-10-31 23:47:08 -0700

    Add NVM settings

--- linux.bashrc.sh
+++ linux.bashrc.sh
@@ -1,3 +1,6 @@
 source $HOME/hunter_dotfiles/common.bashrc.sh
 
 source /usr/share/autojump/autojump.bash
+
+export NVM_DIR="/home/hunter/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
