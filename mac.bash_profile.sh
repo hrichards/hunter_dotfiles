547d990643342c7975dc9417439f68caf8cfe0d5
Hunter Richards <hunter@counsyl.com>
2016-05-15 10:52:40 -0700

    Remove autojump reference

--- mac.bash_profile.sh
+++ mac.bash_profile.sh
@@ -2,6 +2,4 @@ if [ -f `brew --prefix`/etc/bash_completion ]; then
     . `brew --prefix`/etc/bash_completion
 fi
 
-[[ -s `brew --prefix`/etc/autojump.sh ]] && . `brew --prefix`/etc/autojump.sh
-
 source ~/hunter_dotfiles/common.bash_profile.sh
