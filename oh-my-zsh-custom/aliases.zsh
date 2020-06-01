56fc77bb4d7e76b37ab44349ca0121976dbd9623
Hunter Richards <hunter@counsyl.com>
2020-06-01 10:40:08 -0500

    Fix gitgone alias

--- oh-my-zsh-custom/aliases.zsh
+++ oh-my-zsh-custom/aliases.zsh
@@ -1,1 +1,1 @@
-alias gitgone="git branch -vv | grep ': gone]'|  grep -v '\*' | awk '{ print $1; }' | xargs -r git branch -D"
+alias gitgone='git branch -vv | grep ": gone]"|  grep -v "\*" | awk "{ print $0; }" | xargs -r git branch -D'
